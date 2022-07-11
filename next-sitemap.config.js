933439dd35832393814b2b8a62f15d138ffffa17
thutasan <thuta.sann@digitallaboratory.net>
2022-07-11 17:53:44 +0630

    sitemap updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,14 +3,4 @@ const siteUrl = "https://www.thutasann.vercel.app";
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,
-    robotsTxtOptions: {
-        policies: [
-            { userAgent: "*", disallow: "/secret" },
-            { userAgent: "*", allow: "/" },
-        ],
-        additionalSitemaps: [
-        `${siteUrl}/sitemap.xml`,
-        ],
-    },
-    exclude: ["/secret"],
 };
